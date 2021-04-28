d4777d9e347312518972009bb1a63aba2827ed1d
rafael <rafaelds97@hotmail.com>
2021-04-28 15:47:17 -0300

    padawan

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+import { ContactComponent } from './contact/contact.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
+
+const router: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'form', component: FormComponent},
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(router)]
+})
+
+export class AppRoutingModule { }
